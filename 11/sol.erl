ceaa0800753c7e5f4aa9e51d49d8e49323b602dd
Pauls Nartišs <paulsnar@paulsnar.lv>
2025-12-11 11:32:12 +0200

    11p2

--- 11/sol.erl
+++ 11/sol.erl
@@ -9,23 +9,50 @@ parse([Line | Rest], Graph) ->
 	Outputs = string:split(OutputsRaw, " ", all),
 	parse(Rest, Graph#{Node => Outputs}).
 
-visit("out", History, _, _) ->
-	[lists:reverse(["out" | History])];
-visit(Node, History, Visited, Graph) ->
-	case sets:is_element(Node, Visited) of
-		true -> [];
-		false ->
-			Hist1 = [Node | History],
-			Vis1 = sets:add_element(Node, Visited),
-			Neighs = maps:get(Node, Graph, []),
-			visit_children(Neighs, Hist1, Vis1, Graph)
-	end.
-visit_children([], _, _, _) ->
-	[];
-visit_children([Node | Rest], Hist1, Vis1, Graph) ->
-	visit(Node, Hist1, Vis1, Graph) ++
-	visit_children(Rest, Hist1, Vis1, Graph).
-
 sol1(Graph) ->
-	Paths = visit("you", [], sets:new(), Graph),
-	length(Paths).
+	flood_start("you", "out", Graph).
+
+flood_start(Start, Dest, Graph) ->
+	{ok, Deg} = flood([Start], #{}, Dest, Graph),
+	maps:get(Start, Deg).
+
+flood([Dest | Q], Deg, Dest, Graph) ->
+	Deg1 = Deg#{Dest => 1},
+	flood(Q, Deg1, Dest, Graph);
+flood([Node | Q], Deg, Dest, Graph) ->
+	case maps:is_key(Node, Deg) of
+		true -> flood(Q, Deg, Dest, Graph);
+		false ->
+			case flood_pending_neigh(Node, Deg, Graph) of
+				[] ->
+					Deg1 = flood_update(Node, Deg, Graph),
+					flood(Q, Deg1, Dest, Graph);
+				Neigh ->
+					Q1 = Neigh ++ [Node | Q],
+					flood(Q1, Deg, Dest, Graph)
+			end
+	end;
+flood([], Deg, _, _) ->
+	{ok, Deg}.
+
+flood_pending_neigh(Node, Deg, Graph) ->
+	Neighs = maps:get(Node, Graph, []),
+	[Neigh || Neigh <- Neighs, not maps:is_key(Neigh, Deg)].
+flood_update(Node, Deg, Graph) ->
+	case maps:get(Node, Graph, nil) of
+		nil -> Deg#{Node => 0};
+		Neighs ->
+			NodeDeg = lists:sum([maps:get(N, Deg) || N <- Neighs]),
+			Deg#{Node => NodeDeg}
+	end.
+
+sol2(Graph) ->
+	SvrFft = flood_start("svr", "fft", Graph),
+	FftDac = flood_start("fft", "dac", Graph),
+	DacOut = flood_start("dac", "out", Graph),
+
+	SvrDac = flood_start("svr", "dac", Graph),
+	DacFft = flood_start("dac", "fft", Graph),
+	FftOut = flood_start("fft", "out", Graph),
+
+	{SvrFft * FftDac * DacOut, SvrDac * DacFft * FftOut}.
